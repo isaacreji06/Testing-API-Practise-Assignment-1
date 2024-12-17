2447e77f3b4b1dd0f4a0759ebf24e8577063ffde
Isaac Reji <isaac.reji@kalvium.community>
2024-12-17 22:53:33 +0530

    final

--- index.js
+++ index.js
@@ -35,17 +35,33 @@
 const express = require('express');
 const { resolve } = require('path');
 
+const students = require('./data.json');
+
 const app = express();
 const port = 3010;
 
-app.use(express.static('static'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
+app.post('/students/above-threshold', (req, res) => {
+  const { threshold } = req.body;
+
+  if (typeof threshold !== 'number' || threshold < 0) {
+    return res.status(400).json({ error: "Invalid threshold value" });
+  }
+  const filteredStudents = students.filter(student => student.total > threshold);
+  res.json({
+    count: filteredStudents.length,
+    students: filteredStudents
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
 
 
